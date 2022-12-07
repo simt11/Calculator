c9f84469d537f42c5f3a2d6ab5faa6673083ce82
simt11 <simt11@yandex.ru>
2022-12-07 16:11:46 +0300

    Revert "Main"

--- app/src/androidTest/java/com/example/calculator/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/calculator/ExampleInstrumentedTest.kt
@@ -16,11 +16,6 @@ import org.junit.Assert.*
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
     @Test
-    fun name123123() {
-        TODO("Not yet implemented")
-    }
-
-    @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
